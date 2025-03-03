95d5802610748f3e5f1ff9f83fe83c407b9d0efd
Hillary <147104584+Machuge27@users.noreply.github.com>
2025-03-03 09:51:32 +0300

    Delete chat_app directory

--- chat_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import messages_view, chat_page, log_out, getResponse
-
-urlpatterns = [
-    path('user_messages/', messages_view, name='messages_view'),
-    path('messages/', chat_page, name='chat_page'),
-    path('getResponse/', getResponse, name='getResponse'),
-]
