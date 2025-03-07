6d3ebb260b5c3421f13ae71170510ce406f136b7
Machuge27 <mutaihillary3912@gmail.com>
2025-03-07 16:25:54 +0300

    Urls update

--- proxyserver/proxyapp/urls.py
+++ proxyserver/proxyapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.proxy_request, name='proxy'),
-    re_path(r'^(?P<path>.*)$', views.proxy_request, name='proxy_with_path'),
+    path('proxy/', views.proxy_request, name='proxy'),
+    # re_path(r'^(?P<path>.*)$', views.proxy_request, name='proxy_with_path'),
     path("test/", views.test, name='test_request'),
 ]
 
